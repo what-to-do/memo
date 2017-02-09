a774b3700f7bcf2d68588f5f4b697b52911b2de0
Richard Dotchin <richard.dotchin@gmail.com>
2017-02-08 22:58:51 -0500

    test architecture

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 //requireing our models for syncing
 var db = require("./models");
@@ -14,21 +14,30 @@ app.use(express.static("public"));
 //override POST when having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("./public"));
+
+//Routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
 
 //register a Handlebars view engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+/*app.engine('handlebars', exphbs({defaultLayout: 'test'}));
+app.set('view engine', 'handlebars');*/
 
-//require the express routes from burgers_controller.js set to variable routes
-/*var routes = require('./controllers/burgers_controller.js');*/
-//use the var routes (express routes) when url returns /index
-app.use('/', routes);
+
+
 
 
 //syncing our sequlize models and then starting our express app
-db.sequelize.sync({force: true}).then(function(){
+db.sequelize.sync(/*{force: true}*/).then(function(){
 	app.listen(PORT, function(){
 	console.log("listenning on http://localhost:" + PORT);
 });
