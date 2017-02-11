ee0052102acb3f3ffbb2bc1715ec5ab58fc2f33d
Richard Dotchin <richard.dotchin@gmail.com>
2017-02-11 18:07:01 -0500

    more sequelize edits

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,9 @@ var path = require('path');
 
 module.exports = function(app){
 	app.get('/api/view', function(req, res){
-		db.Categories.findAll().then(function(data){
+		db.Categories.findAll({
+			/*include: [db.Snippets]*/
+		}).then(function(data){
 			console.log('\nfindall categories data\n');
 			res.json(data);
 			console.log(data);
@@ -11,6 +13,12 @@ module.exports = function(app){
 			console.log("\ncategories find all error\n");
 			console.log(err);
 		});
+
+		/*db.Snippets.findAll().then(function(data){
+			res.json(data);
+		}).catch(function(err){
+			console.log(err);
+		});*/
 	});
 	
 	app.post('/api/add', function(req, res){
@@ -18,7 +26,7 @@ module.exports = function(app){
 			category: req.body.category
 		}).then(function(data){
 			res.json(data);
-			console.log(data);
+			/*console.log(data);*/
 		}).catch(function(err){
 			console.log("\ncategories create error\n");
 			console.log(err);
@@ -28,7 +36,8 @@ module.exports = function(app){
 			snippet: req.body.snippet,
 			importance: req.body.urgency,
 		}).then(function(data){
-			console.log(data);
+			res.redirect('/api/view');
+			/*console.log(data);*/
 		}).catch(function(err){
 			console.log('\nsnippet create error\n');
 			console.log(err);
