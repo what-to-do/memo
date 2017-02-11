98c1905eb701823850ad200b2950d489364d4cac
Jessica Prado <jessica.marie.prado@gmail.com>
2017-02-11 18:49:05 -0500

    category buttons and collapse work

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,4 @@
-
-//dynamic categories
-//
 $(document).ready(function(){
-
 
 var categories = ["music", "movies", "recipes"];
 
@@ -12,14 +8,14 @@ for (var i = 0; i < categories.length; i++) {
   cat_btn.attr({
     "class" : "btn btn-primary",
     "type" : "button",
-    "data-toogle" : "collapse",
+    "data-toggle" : "collapse",
     "data-target" : "#collapseExample",
     "aria-expanded": false,
     "aria-controls": "collapseExample",
     "data-index" : i
     });
   cat_btn.appendTo(".categories");
-
+};  
 
   var cat_div = $("<div>");
   cat_div.attr({
@@ -27,176 +23,17 @@ for (var i = 0; i < categories.length; i++) {
     "id" : "collapseExample",
     "aria-expanded": false,
   });
-  cat_div.appendTo(".content")
-
-  var cat_block = $("<div>");
-  cat_block.attr ({
-    "class" : "card card-block"
-  });
-  cat_block.appendTo(cat_div);
-
-  var cat_para = $('<p>');
-  cat_para.text("Hello World");
-  cat_para.appendTo(cat_block)
-};  
- 
-  
+  cat_div.text("hello");
+  cat_div.appendTo(".content");
 
 
+$(".add_snippet").on("click", function() {
+  $('#snippet').modal('toggle'); 
+})
 
-// Get the modal
-//var modal = $('#myModal');
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-//var modal_btn = $('#modal_btn');
-var modal_btn = document.getElementById("modal_btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-
-modal_btn.onclick = function() {
-    modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-  $('.collapse').collapse();
+  $('.collapse').collapse('toggle');
 
 
 
 });
-
-/*
-$('.datepicker').pickadate({// Strings and translations
-monthsFull: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-weekdaysFull: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-weekdaysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-showMonthsShort: undefined,
-showWeekdaysFull: undefined,
-
-// Buttons
-today: 'Today',
-clear: 'Clear',
-close: 'Close',
-
-// Accessibility labels
-labelMonthNext: 'Next month',
-labelMonthPrev: 'Previous month',
-labelMonthSelect: 'Select a month',
-labelYearSelect: 'Select a year',
-
-// Formats
-format: 'd mmmm, yyyy',
-formatSubmit: undefined,
-hiddenPrefix: undefined,
-hiddenSuffix: '_submit',
-hiddenName: undefined,
-
-// Editable input
-editable: undefined,
-
-// Dropdown selectors
-selectYears: undefined,
-selectMonths: undefined,
-
-// First day of the week
-firstDay: undefined,
-
-// Date limits
-min: undefined,
-max: undefined,
-
-// Disable dates
-disable: undefined,
-
-// Root picker container
-container: undefined,
-
-// Hidden input container
-containerHidden: undefined,
-
-// Close on a user action
-closeOnSelect: true,
-closeOnClear: true,
-
-// Events
-onStart: undefined,
-onRender: undefined,
-onOpen: undefined,
-onClose: undefined,
-onSet: undefined,
-onStop: undefined,
-
-// Classes
-klass: {
-
-  // The element states
-  input: 'picker__input',
-  active: 'picker__input--active',
-
-  // The root picker and states *
-  picker: 'picker',
-  opened: 'picker--opened',
-  focused: 'picker--focused',
-
-  // The picker holder
-  holder: 'picker__holder',
-
-  // The picker frame, wrapper, and box
-  frame: 'picker__frame',
-  wrap: 'picker__wrap',
-  box: 'picker__box',
-
-  // The picker header
-  header: 'picker__header',
-
-  // Month navigation
-  navPrev: 'picker__nav--prev',
-  navNext: 'picker__nav--next',
-  navDisabled: 'picker__nav--disabled',
-
-  // Month & year labels
-  month: 'picker__month',
-  year: 'picker__year',
-
-  // Month & year dropdowns
-  selectMonth: 'picker__select--month',
-  selectYear: 'picker__select--year',
-
-  // Table of dates
-  table: 'picker__table',
-
-  // Weekday labels
-  weekdays: 'picker__weekday',
-
-  // Day states
-  day: 'picker__day',
-  disabled: 'picker__day--disabled',
-  selected: 'picker__day--selected',
-  highlighted: 'picker__day--highlighted',
-  now: 'picker__day--today',
-  infocus: 'picker__day--infocus',
-  outfocus: 'picker__day--outfocus',
-
-  // The picker footer
-  footer: 'picker__footer',
-
-  // Today, clear, & close buttons
-  buttonClear: 'picker__button--clear',
-  buttonClose: 'picker__button--close',
-  buttonToday: 'picker__button--today'
-}});*/
