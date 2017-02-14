d1a613e1e77d3b6902b60e10a97cb1da8c9f3eab
Jessica Prado <jessica.marie.prado@gmail.com>
2017-02-13 21:18:13 -0500

    routing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
 var categories = ["music", "movies", "recipes"];
 
 for (var i = 0; i < categories.length; i++) {
@@ -26,18 +27,202 @@ for (var i = 0; i < categories.length; i++) {
   cat_div.text("This is test text.Blah blah blah");
   cat_div.appendTo(".content");
 
-$('.submit').on('click', function() {
-  var newMemo = {
-    snippet : $('#snippet_modal').val(),
-    category : $('#selected_category :selected').text(),
-    urgency: $("input[name='group1']:checked").val()
-    };
 
-    $.post("/api/add", newMemo)
+    viewing();
+
+
+    function crud(){
+
+
+      $(".del").on("click" , function(event){
+      
+        console.log("obj");
+        var user_delete = $(this).data("index");
+
+        console.log(user_delete);
+
+        $.post("/api/delete" , {user_delete: user_delete}).
+        done(function(data){
+
+          viewing();
+
+        });
+
+      });
+
+
+      // Question: What does this code do?
+      $("#add-button").on("click", function(event) {
+        event.preventDefault();
+
+        var new_category = {
+
+          snippet: $("#snippet").val().trim(),
+          category: $("#category").val().trim(),
+          urgency: $("input[name='group1']:checked").val()
+      
+
+        };
+
+        console.log(new_category);
+
+        // Question: What does this code do??
+        $.post("/api/add", new_category)
         .done(function(data) {
           console.log(data);
-    });
 
-});
+          viewing();
 
+        });
+      });
+
+
+    }
+
+    
+    function viewing(){
+
+      $.get("/api/view", function(data) {
+          console.log(data);
+          console.log("hello");
+          $("#content").html("");
+
+          var table = $("<table>");
+
+          table.addClass("table");
+
+          table.appendTo("#content");
+
+          var thead = $("<thead>");
+
+          thead.appendTo(table);
+
+          var tr = $("<tr>");
+
+          tr.appendTo(thead);
+
+          var heading = ["#","Created","Snippet","Category" , "Urgency","Actions"];
+
+          for (let i = 0; i < heading.length; i++) {
+            
+            var th = $("<th>");
+
+            th.text(heading[i]);
+
+            th.appendTo(tr);
+
+          } // End of For Loop I
+          
+          var tbody = $("<tbody>");
+
+          tbody.appendTo(table);
+
+          for (let i = 0; i < data.length; i++) {
+
+            var tr2 = $("<tr>");
+
+            tr2.appendTo(tbody);
+            
+            let th = $("<th>");
+
+            th.attr({
+              "scope": i + 1
+            });
+
+            th.text(i);
+
+            th.appendTo(tr2);
+
+
+
+            let td_created = $("<td>");
+
+            td_created.text(data[i].createdAt);
+
+            td_created.appendTo(tr2);
+
+
+            let td_snippet = $("<td>");
+
+            td_snippet.text(data[i].snippet);
+
+            td_snippet.appendTo(tr2)
+
+
+
+            let td_category = $("<td>");
+
+            td_category.text(data[i].Category.category);
+
+            td_category.appendTo(tr2);
+
+
+
+
+            let td_import = $("<td>");
+
+            td_import.text(data[i].importance);
+
+            td_import.appendTo(tr2);
+
+
+
+            let td_action = $("<td>");
+
+            td_action.appendTo(tr2);
+
+
+            var a_edit = $("<a>");
+
+            var i_edit = $("<i>");
+
+            a_edit.attr({
+
+              "class": "teal-text edit",
+                        "data-index": data[i].id
+
+            });
+
+            i_edit.attr({
+
+              "class" : "fa fa-pencil"
+
+            });
+
+            var a_delete = $("<a>");
+
+            var i_delete = $("<i>");
+
+            a_delete.attr({
+
+              "class": "red-text del",
+                        "data-index": data[i].id
+
+            });
+
+            i_delete.attr({
+
+              "class" : "fa fa-times"
+
+            });
+
+            a_edit.appendTo(td_action);
+            i_edit.appendTo(a_edit);
+
+
+            a_delete.appendTo(td_action);
+            i_delete.appendTo(a_delete);
+
+          } // End of For Loop I
+          
+
+       // End of For Loop I
+          crud();
+
+        });
+
+      
+
+     }
 });
+  
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/assets/html/test.html"));
+    res.sendFile(path.join(__dirname + "/../public/assets/html/index.html"));
   });
 
  
